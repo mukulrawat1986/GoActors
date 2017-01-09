dbd1f18a92ad7fd8e7ab00f9ace73b723888123b
Mukul Rawat <mukulsrawat@gmail.com>
2017-01-09 19:48:57 +0530

    Test for AskForNames for more than two values

--- main_test.go
+++ main_test.go
@@ -40,3 +40,20 @@ func Test_AskForNames(t *testing.T) {
 	a.Equal(main.ActorNames[0], "Brad")
 	a.Equal(main.ActorNames[1], "Pitt")
 }
+
+func Test_AskForNames_FourNames(t *testing.T) {
+	setup()
+
+	a := assert.New(t)
+	b := []byte("Brad\nPitt\nJennifer\nLopez\n")
+
+	r := bytes.NewBuffer(b)
+
+	main.AskForNames(r)
+
+	a.Equal(len(main.ActorNames), 4)
+	a.Equal(main.ActorNames[0], "Brad")
+	a.Equal(main.ActorNames[1], "Pitt")
+	a.Equal(main.ActorNames[2], "Jennifer")
+	a.Equal(main.ActorNames[3], "Lopez")
+}
